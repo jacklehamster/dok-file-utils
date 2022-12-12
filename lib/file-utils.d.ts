a5bd1b172995a32005b478bfbfe898b07cf6f519
Jack Le Hamster <jacklehamster@gmail.com>
2022-12-12 07:46:50 -0800

    setOverride in typescript. Issues fixed https://github.com/microsoft/TypeScript/issues/51567

--- lib/file-utils.d.ts
+++ lib/file-utils.d.ts
@@ -2,6 +2,8 @@ export class FileUtils {
     constructor(XMLHttpRequest: any);
     XMLHttpRequest: any;
     fileStock: {};
+    setOverride(override: any): void;
+    override: any;
     preload(...urls: any[]): Promise<any[]>;
     load(url: any, responseType: any): Promise<any>;
 }
